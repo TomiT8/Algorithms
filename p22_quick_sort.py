9eaa4a41a7c7be21c175ede5593b970641456d99
TomiT8 <tomas.jasko8@gmail.com>
2024-08-20 18:42:36 +0200

    pridaný p22

---
+++ p22_quick_sort.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+def quick_sort(unsorted_list: List[int]) -> List[int]:
+    # print(unsorted_list)
+    if len(unsorted_list) <= 1:
+        return unsorted_list
+
+    mid = unsorted_list[0]
+
+    left_half = []
+    right_half = []
+    middle = []
+
+    for x in unsorted_list:
+        if x < mid:
+            left_half.append(x)
+        elif x > mid:
+            right_half.append(x)
+        else:
+            middle.append(x)
+
+    return quick_sort(left_half) + middle + quick_sort(right_half)
+
+
+if __name__ == "__main__":
+    unsorted_list = [8, 4, 2, 5, 3, 1, 7, 2, 9]
+    print(quick_sort(unsorted_list))
