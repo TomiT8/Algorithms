ad929c2baebb6d766a16e503ff286ede01a877d2
TomiT8 <tomas.jasko8@gmail.com>
2024-08-20 20:55:49 +0200

    p25 doplnený aj o merge

--- p25_String_lenght_sort.py
+++ p25_String_lenght_sort.py
@@ -3,6 +3,7 @@ zadanie: Usporaidať texty v zozname podľa ich dĺžky:
 ["Ahoj", "Hello", "Hi", "Nazdar", "Čau"] -> ["Hi", "Čau", "Ahoj", "Hello", "Nazdar"]
 """
 
+"""quick sort"""
 def string_lenght_sort(arr):
     if len(arr) <= 1:
         return arr
@@ -14,6 +15,41 @@ def string_lenght_sort(arr):
     right_half = [x for x in arr if len(x) > pivot]
 
     return string_lenght_sort(left_half) + middle + string_lenght_sort(right_half)
+
+
+if __name__ == "__main__":
+    unsorted_list = ["Ahoj", "Hello", "Hi", "Nazdar", "Čau"]
+    print(string_lenght_sort(unsorted_list))
+
+
+""" merge sort"""
+
+
+def string_lenght_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left_half = string_lenght_sort(arr[:mid])
+    right_half = string_lenght_sort(arr[mid:])
+
+    # print(f"{left_half}, {right_half}")
+
+    sorted_list = []
+    left_index, right_index = 0, 0
+
+    while left_index < len(left_half) and right_index < len(right_half):
+        if len(left_half[left_index]) < len(right_half[right_index]):
+            sorted_list.append(left_half[left_index])
+            left_index += 1
+        else:
+            sorted_list.append(right_half[right_index])
+            right_index += 1
+
+    sorted_list.extend(left_half[left_index:])
+    sorted_list.extend(right_half[right_index:])
+
+    return sorted_list
 
 
 if __name__ == "__main__":
---
+++ p26_2D_points.py
@@ -0,0 +1,13 @@
+"""
+Exercise H:
+Třídění bodů v rovině (2D bodů)
+Napište program, který seřadí seznam bodů v rovině podle jejich vzdálenosti od počátku (0,0). Body budou reprezentovány souřadnicemi v tupple:
+[(1, 0), (1, 1), (2, 5), (3, 2), (4, 5), (0, 1), (5, 1)]
+BONUS: Pokud mají dva body stejnou vzdálenost, seřaďte je podle jejich úhlu vzhledem k ose X.
+================================================
+Exercise I:
+Třídění kladných celých čísel podle počtu celočíselných dělitelů:
+[5, 8, 12, 16, 1, 17, 30, 3, 9, 30, 101, 101] -> [1, 3, 5, 17, 101, 101, 9, 8, 16, 12, 30, 30]
+BONUS: Optimalizujte hledání pomocí dynamického programování tak, abyste pro zadané číslo hledali počet dělitelů jenom jednou.
+"""
+
